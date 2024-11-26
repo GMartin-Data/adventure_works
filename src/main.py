ad27fb89e3c39aa296ac9e73da5151600a7b5c30
Greg <gregory.martin.data@gmail.com>
2024-11-26 14:18:11 +0100

    Ruff formatting

--- src/main.py
+++ src/main.py
@@ -38,13 +38,13 @@ def generate_sas_token(datalake, account_key, container_name):
             account_key=account_key,
             container_name=container_name,
             permission=ContainerSasPermissions(read=True, list=True),
-            expiry=datetime.now(timezone.utc) + timedelta(hours=1)
+            expiry=datetime.now(timezone.utc) + timedelta(hours=1),
         )
         return sas_token
     except Exception as e:
         logging.error(f"SAS Error : {e}")
         raise
-    
+
 
 def generate_sas_url() -> str:
     datalake = os.getenv("DATALAKE")
@@ -58,37 +58,36 @@ def list_blobs_with_etension(container_url, file_extension="parquet") -> List[st
     try:
         container_client = ContainerClient.from_container_url(container_url)
         blobs = container_client.list_blobs()
-        blob_names = [blob.name
-                      for blob in blobs
-                      if blob.name.endswith(f".{file_extension}")]
+        blob_names = [
+            blob.name for blob in blobs if blob.name.endswith(f".{file_extension}")
+        ]
         return blob_names
     except Exception as e:
         logging.error(f"Error when generating blob names' list: {e}")
         raise
-    
+
 
 def download_parquet_with_sas(container_url, blob_name, download_path):
     try:
         container_client = ContainerClient.from_container_url(container_url)
         blob_client = container_client.get_blob_client(blob_name)
-        
+
         os.makedirs(os.path.dirname(download_path), exist_ok=True)
         with open(download_path, "wb") as file:
             file.write(blob_client.download_blob().readall())
-            
+
         logging.info(f"Downloaded blob: {blob_name} -> {download_path}")
     except Exception as e:
         logging.error(f"Error during download of {blob_name}: {e}")
         raise
-    
+
 
 if __name__ == "__main__":
     full_url = generate_sas_url()
-    
+
     blobs_list = list_blobs_with_etension(full_url)
     logging.info(f"List of parquet files: {blobs_list}")
-    
+
     for blob in tqdm(blobs_list, desc="Parquet Files Download...", unit="file"):
         download_path = f"./data/{blob}"
         download_parquet_with_sas(full_url, blob, download_path)
-        
