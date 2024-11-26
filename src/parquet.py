7d543343ed395ad77d81027f505e95673bee5d39
Greg <gregory.martin.data@gmail.com>
2024-11-26 14:18:34 +0100

    Ruff formatting

--- src/parquet.py
+++ src/parquet.py
@@ -8,16 +8,12 @@ import polars as pl
 from rich import print as rprint
 
 
-lazy_df = (pl
-           .scan_parquet("data/product_eval/test-00002-of-00003.parquet")
-        #    .limit(10)
+lazy_df = (
+    pl.scan_parquet("data/product_eval/test-00002-of-00003.parquet")
+    #    .limit(10)
 )
 
-csv_fields = (lazy_df
-              .collect_schema()
-              .names()
-              [1:]
- )
+csv_fields = lazy_df.collect_schema().names()[1:]
 
 images_folder = "data/parquet/parquet_images/"
 csv_file = "data/parquet/parquet_metadata.csv"
@@ -29,13 +25,15 @@ os.makedirs(images_folder, exist_ok=True)
 with open(csv_file, mode="w", newline="", encoding="utf-8") as csv_writefile:
     csv_writer = csv.writer(csv_writefile)
     csv_writer.writerow(csv_fields)  # Writing CSV file header
-    
+
     # Process the lazy DataFrame
     for idx, row in enumerate(lazy_df.collect().iter_rows(named=True)):
         # Write metadata on CSV
-        csv_writer.writerow([row["item_ID"], row["query"], row["title"], row["position"]])
+        csv_writer.writerow(
+            [row["item_ID"], row["query"], row["title"], row["position"]]
+        )
         # Decode and save image as PNG
-        try: 
+        try:
             img = row["image"]["bytes"]
             # print(type(img))
             # input("CONTINUE?")
@@ -44,7 +42,7 @@ with open(csv_file, mode="w", newline="", encoding="utf-8") as csv_writefile:
                 image.save(os.path.join(images_folder, f"{idx}.png"), format="PNG")
         except Exception as e:
             print(f"Error processing image at index: {idx}:\n{e}")
-    
+
 # rprint(lazy_df
 #        .limit(10)
 #        .collect()
