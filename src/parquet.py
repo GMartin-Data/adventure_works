958606e71ad519ce04215d793acf8837d7a58e83
Greg <gregory.martin.data@gmail.com>
2024-11-27 10:39:06 +0100

    Add logger and progress bar to parquet processing

--- src/parquet.py
+++ src/parquet.py
@@ -1,32 +1,50 @@
-import base64
 import csv
 import io
+import logging
 import os
 
 from PIL import Image
 import polars as pl
-from rich import print as rprint
+from tqdm import tqdm
 
+# Configure logger
+logging.basicConfig(
+    filename="./logs/parquet_processing.log",
+    level=logging.INFO,
+    format="%(name)s - %(asctime)s - %(levelname)s - %(message)s",
+)
 
-for file in os.listdir("data/product_eval"):
-    lazy_df = pl.scan_parquet(f"data/product_eval/{file}")
+# Logger instanciation
+logging.getLogger(__name__)
 
-    csv_fields = lazy_df.collect_schema().names()[1:]
+# Set files and folder
+images_folder = "data/parquet/parquet_images/"
+csv_file = "data/parquet/parquet_metadata.csv"
+os.makedirs(images_folder, exist_ok=True)
 
-    images_folder = "data/parquet/parquet_images/"
-    csv_file = "data/parquet/parquet_metadata.csv"
+# Open the CSV file for writing
+with open(csv_file, mode="w", newline="", encoding="utf-8") as csv_writefile:
+    csv_writer = csv.writer(csv_writefile)
+    csv_fields_written = False  # Flag to ensure CSV fields are only written once
 
-    # Create output folders if not exists
-    os.makedirs(images_folder, exist_ok=True)
+    # Iterate over all Parquet files
+    for file in os.listdir("data/product_eval"):
+        lazy_df = pl.scan_parquet(f"data/product_eval/{file}")
 
-    # Manage metadata in CSV file
-    with open(csv_file, mode="w", newline="", encoding="utf-8") as csv_writefile:
-        csv_writer = csv.writer(csv_writefile)
-        csv_writer.writerow(csv_fields)  # Writing CSV file header
+        # Collect the schema once, then the data
+        if not csv_fields_written:
+            csv_fields = lazy_df.collect_schema().names()[1:]
+            csv_writer.writerow(csv_fields)  # Writing CSV file header
+            csv_fields_written = True
 
-        # Process the lazy DataFrame
-        for idx, row in enumerate(lazy_df.collect().iter_rows(named=True)):
-            # Write metadata on CSV
+        df = lazy_df.collect()
+        total_rows = df.height
+
+        # Process the DataFrame with a progress bar
+        for row in tqdm(
+            df.iter_rows(named=True), total=total_rows, desc=f"Processing: '{file}'"
+        ):
+            # Extract image name and write metadata on CSV
             image_name = row["image"]["path"].split(".")[0]
             csv_writer.writerow(
                 [
@@ -34,13 +52,16 @@ for file in os.listdir("data/product_eval"):
                     row["query"],
                     row["title"],
                     row["position"],
-                    image_name
+                    image_name,
                 ]
             )
             # Decode and save image as PNG
             try:
                 img = row["image"]["bytes"]
                 with Image.open(io.BytesIO(img)) as image:
-                    image.save(os.path.join(images_folder, f"{image_name}.png"), format="PNG")
+                    image.save(
+                        os.path.join(images_folder, f"{image_name}.png"), format="PNG"
+                    )
             except Exception as e:
-                print(f"Error processing image at index: {idx}:\n{e}")
+                logging.error(f"❌ Error processing image '{image_name}':\n{e}")
+        logging.info(f"✅ Successfully processed file: '{file}'")
